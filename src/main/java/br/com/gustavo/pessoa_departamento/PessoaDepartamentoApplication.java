39e475db1bc69af78fd708ffb63d988c30c829f6
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-11 16:44:14 -0300

    projeto criado

---
+++ src/main/java/br/com/gustavo/pessoa_departamento/PessoaDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.pessoa_departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaDepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaDepartamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/pessoa_departamento/PessoaDepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.pessoa_departamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaDepartamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
