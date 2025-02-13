6df5ef0b520017c48718e651d37d4727fd1f7062
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-02-13 01:09:53 -0300

    properties e banco h2 ok

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_departamento (nome) VALUES ('T.I');
+INSERT INTO tb_departamento (nome) VALUES ('Diretoria');
+INSERT INTO tb_departamento (nome) VALUES ('Fiscalização');
+
+INSERT INTO tb_pessoa (nome, salario, departamento_id) VALUES ('Gustavo', '8000.0', 1);
+INSERT INTO tb_pessoa (nome, salario, departamento_id) VALUES ('Gustavo', '8000.0', 2);
+INSERT INTO tb_pessoa (nome, salario, departamento_id) VALUES ('Gustavo', '8000.0', 3);
